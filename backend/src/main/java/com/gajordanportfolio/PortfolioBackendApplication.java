8ed85d6438f8e25dbe84fc717d47ac52ea2e097d
alexjordan98 <alexjordan98@gmail.com>
2025-07-15 17:11:11 -0400

    Updated project configuration

--- backend/src/main/java/com/gajordanportfolio/PortfolioBackendApplication.java
+++ backend/src/main/java/com/gajordanportfolio/PortfolioBackendApplication.java
@@ -2,12 +2,17 @@ package com.gajordanportfolio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"controllers", "services", "core", "com.gajordanportfolio"})
+@EntityScan(basePackages = {"core.entities"})
+@EnableJpaRepositories(basePackages = {"core.repositories"})
 public class PortfolioBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PortfolioBackendApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(PortfolioBackendApplication.class, args);
+    }
 }
